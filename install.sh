7545d8a7ef7fb0751f2e7b8270743c48b86fcd22
Simon-Sonnleitner <jim48@gmx.de>
2025-05-09 15:03:37 +0200

    improved install

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ rm -rf ~/.config/nvim/
 # cp $PWD/nvim/ ~/.config/
 # echo $PWD
 #
-git clone https://github.com/KyNarec/neovim_config.git ~/.config/nvim
-cd ~/.config/nvim/
+git clone https://github.com/KyNarec/neovim_config.git ~/.config/
+cd ~/.config/
+rm -rf ./README.md
+rm -rf ./install.sh
+rm -rf ./.git/
