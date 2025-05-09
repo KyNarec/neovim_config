b3b484bf04c8d98e0a7ab17ddb0537f00e395396
Simon-Sonnleitner <jim48@gmx.de>
2025-05-09 14:53:09 +0200

    changed install logic

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 rm -rf ~/.config/nvim/
-cp $PWD/nvim/ ~/.config/
-echo $PWD
+# cp $PWD/nvim/ ~/.config/
+# echo $PWD
+#
+git clone https://github.com/KyNarec/neovim_config.git ~/.config/nvim
+cd ~/.config/nvim/
